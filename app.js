aa519710c19e2395114106fbdf2b5a89229b5021
croustibzh <m.ledot@outlook.com>
2020-10-09 16:46:29 -0400

    Final application version

--- app.js
+++ app.js
@@ -20,8 +20,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const nexmo = new Nexmo(
   {
-    apiKey: "7e4923fb",
-    apiSecret: "eVydBwxSAKLnk6j7",
+    // Your unique API key and secret
+    apiKey: "",
+    apiSecret: "",
   },
   { debug: true }
 );
@@ -33,14 +34,12 @@ app.get("/", (req, res) => {
 
 // Catch form submit
 app.post("/", (req, res) => {
-  // res.send(req.body);
-  // console.log(req.body);
 
   const number = req.body.number;
   const text = req.body.text;
 
   nexmo.message.sendSms(
-    "15877504075",
+    "15877504075", // Virtual phone number (provided on your Vonage account)
     `1${number}`,
     text ,
     (err, responseData) => {
